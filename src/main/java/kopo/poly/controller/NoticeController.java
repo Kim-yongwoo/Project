95243b0e2728a06d4493a64dc349628f07559c83
YongKim-creator <redgum0319@naver.com>
2022-04-04 15:09:50 +0900

    글 수정

--- src/main/java/kopo/poly/controller/NoticeController.java
+++ src/main/java/kopo/poly/controller/NoticeController.java
@@ -36,7 +36,7 @@ public class NoticeController {
     private INoticeService noticeService;
 
     /**
-     * GetMapping은 GET방식을 통해 접속되는 URL 호출에 대해 실행되는 함수로 설정함을 의미함
+     * GetMapping은 GET방식을 통해 접속되는 URL 호출에 대해 실행되는 함수로 설정함을 의미함이야
      * PostMapping은 POST방식을 통해 접속되는 URL 호출에 대해 실행되는 함수로 설정함을 의미함
      * GetMapping(value = "index") =>  GET방식을 통해 접속되는 URL이 index인 경우 아래 함수를 실행함
      */
